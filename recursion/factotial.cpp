307d60eb762ae5f0ef345c6d6ca34e097faa8ace
samridh rana <samridhrana06@gmail.com>
2024-01-31 11:43:07 +0530

    recursion in cpp

---
+++ recursion/factotial.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fact(int n){
+
+    if(n==0 or n == 1) return 1;
+
+    return fact(n-1)*n;
+    
+    }
+
+    int main(){
+
+        int r;
+        r = fact(5);
+        cout<<r;
+        return 0;
+    }
---
+++ recursion/sum_of_natural_number.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// int sum(int n){
+
+//     if(n==0) return 0;
+//     return sum(n-1)+n;
+// }
+
+int sum(int n){
+    
+}
+
+int main(){
+
+int r;
+r = sum(5);
+cout<<r;
+
+    return 0;
+}
